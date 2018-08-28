8a57d8312c52834863192f28515c74c9eadd6e94
Artur O <artur_odekov@yahoo.com>
2018-08-28 15:12:37 +0300

    secondary

--- routes/uploads.js
+++ routes/uploads.js
@@ -29,23 +29,42 @@ router.get('/', (req, res) => [
 ])
 // gallery
 router.get('/gallery', (req, res) => {
-    Image.find().then(image => {
-        console.log(image)
+    Image.find({
+
+    }).then(image => {
+        // console.log(image.img.contentType)
+        //console.log(image[0].img.data.toString('base64'))
+        // res.render('uploads/gallery', {
+        //     image: image[0].img.data.toString('base64')
+        // })
+
+        // res.setHeader('content-type', image[0].img.contentType);
+        // let imageArray = []
+        // let getBuffer = pic.forEach(element => {
+
+        //     imageArray.push(Buffer.from(element.img.data).toString('base64'))
+        // })
+
+        // var thumb = Buffer.from(element.img.data).toString('base64')
+
         res.render('uploads/gallery', {
             image: image
         })
+        // res.send(image[0].img.data)
     })
 })
 
 //uploads
 router.post('/', upload.single('img'), (req, res) => {
-    const picture = new Image();
+    const picture = new Image;
     // ({
     //     img: req.file.path
     // })
 
+
     picture.img.data = fs.readFileSync(req.file.path);
-    picture.img.contentType = 'image/jpg';
+    picture.img.contentType = req.file.mimetype;
+    picture.title = req.file.filename;
 
     picture.save().then((image) => {
         res.redirect('uploads/gallery')
@@ -58,7 +77,7 @@ router.delete('/delete/:id', (req, res) => {
         _id: req.params.id
     }).then(image => {
         image.remove();
-        fs.unlink(image.img, () => {
+        fs.unlink(image.img.data, () => {
             console.log('File deleted')
         })
         res.redirect('/uploads/gallery')
